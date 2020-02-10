71746d1c5e9956daf673800ad3f1c88c961df1db
Otavio Cardoso Borges <otavio.car.borges@gmail.com>
2020-02-10 10:38:17 -0300

    Adding suppport for UDP sockets

--- inc/udp.h
+++ inc/udp.h
@@ -18,9 +18,12 @@ typedef struct{
 	uint16_t destPort;
 	uint16_t length;
 	uint16_t checksum;
-	uint8_t options[40];
 }udp_header_t;
 
+typedef int (*udp_callback_t)(uint8_t *, uint16_t , uint8_t *);
+
 int UDP_ProcessPacket(uint8_t *msg, uint16_t length, uint8_t *reply, uint8_t *sourceMaC, uint32_t *replyOnBroadcast);
+int UDP_GetHeader(uint8_t *payload, uint16_t dataLength, uint16_t destPort, uint16_t srcPort);
+int UDP_SendPacket(uint32_t destination, uint8_t *data, uint16_t srcPort, uint16_t destPort, uint32_t length, uint8_t *payload, udp_callback_t callback);
 
 #endif /* UDP_H_ */
--- udp.c
+++ udp.c
@@ -13,6 +13,10 @@
 
 #include "udp.h"
 #include "dhcp.h"
+#include "ntp.h"
+
+static udp_callback_t g_callback = NULL;
+static uint16_t g_callbackPort = 0;
 
 int UDP_ProcessPacket(uint8_t *msg, uint16_t length, uint8_t *reply, uint8_t *sourceMaC, uint32_t *replyOnBroadcast){
 	udp_header_t *udp;
@@ -35,17 +39,56 @@ int UDP_ProcessPacket(uint8_t *msg, uint16_t length, uint8_t *reply, uint8_t *so
 	msg += PACKET_UDP_LENGTH;
 	length -= PACKET_UDP_LENGTH;
 
+#if (DYN_CONF_DHCP_CLIENT == 1)
+	if((udp->destPort == DHCP_SERVER_PORT) || (udp->destPort == DHCP_CLIENT_PORT)){
+#else
 	if(udp->destPort == DHCP_SERVER_PORT){
+#endif
 		replyLength = DHCP_Parse(sourceMaC, (dhcp_header_t *)msg, payload, replyOnBroadcast);
-
-		if(replyLength == 0)
-			return 0;
-
-		replyUdp->length = REVERT_16BITS((replyLength + PACKET_UDP_LENGTH));
-		return (replyLength + PACKET_UDP_LENGTH);
+	}else if(udp->destPort == REVERT_16BITS(NTP_UDP_PORT)){
+		replyLength = NTP_Parse((ntp_header_t *)msg, payload);
+	}else if(udp->destPort == REVERT_16BITS(g_callbackPort)){
+		if(g_callback != NULL){
+			replyLength = (*g_callback)(msg, length, payload);
+			g_callback = NULL;
+			g_callbackPort = 0;
+		}else{
+			return 0; // port not supported
+		}
 	}
 
-	return 0; // we don't support that protocol
+	if(replyLength == 0)
+		return 0;
+
+	replyUdp->length = REVERT_16BITS((replyLength + PACKET_UDP_LENGTH));
+	return (replyLength + PACKET_UDP_LENGTH);
 }
 
+int UDP_GetHeader(uint8_t *payload, uint16_t dataLength, uint16_t destPort, uint16_t srcPort){
+	udp_header_t *udp = (udp_header_t *)payload;
 
+	udp->sourcePort = REVERT_16BITS(srcPort);
+	udp->destPort = REVERT_16BITS(destPort);
+	udp->checksum = 0;
+	udp->length = REVERT_16BITS((dataLength + PACKET_UDP_LENGTH));
+
+	return PACKET_UDP_LENGTH;
+}
+
+int UDP_SendPacket(uint32_t destination, uint8_t *data, uint16_t srcPort, uint16_t destPort, uint32_t length, uint8_t *payload, udp_callback_t callback){
+	uint32_t totalLength = 0;
+	uint8_t *macDest;
+
+	macDest = IPV4_GetMAC(destination);
+	totalLength = ETH_CreateHeader(payload, macDest, ETH_TYPE_IPV4);
+	totalLength += IPV4_GetHeader((payload + totalLength), (length + PACKET_UDP_LENGTH), destination, IPv4_PROTOCOL_UDP);
+	totalLength += UDP_GetHeader((payload + totalLength), length, destPort, srcPort);
+
+	memcpy((payload + totalLength), data, length);
+
+	if(callback != NULL){
+		g_callbackPort = srcPort;
+		g_callback = callback;
+	}
+	return (totalLength + length);
+}
